ba4822572aa186a8cc0681512ef10a378e7ec5ee
Chris Watts <devgrok@users.noreply.github.com>
2015-09-22 21:12:29 +1000

    Allow skipActiveLabelTypes, skipLabelTypes, skipInputType to be easily modified

--- components/afFormGroup/afFormGroup.js
+++ components/afFormGroup/afFormGroup.js
@@ -1,61 +1,64 @@
+Template.afFormGroup_materialize.skipInputType = [
+    'checkbox',
+    'checkbox-group',
+    'boolean-checkbox',
+    'select-radio',
+    'select-checkbox-inline',
+    'select-radio-inline',
+    'boolean-radios',
+    'toggle',
+    'switch'
+];
+Template.afFormGroup_materialize.skipLabelTypes = [
+    'checkbox',
+    'checkbox-group',
+    'boolean-checkbox',
+    'select-radio',
+    'select-checkbox-inline',
+    'select-radio-inline',
+    'boolean-radio',
+    'toggle',
+    'switch'
+];
 Template.afFormGroup_materialize.helpers({
-    addInputField: function() {
+    addInputField: function () {
         var result, skipInputType, type;
-        skipInputType = [
-            'checkbox',
-            'checkbox-group',
-            'boolean-checkbox',
-            'select-radio',
-            'select-checkbox-inline',
-            'select-radio-inline',
-            'boolean-radios',
-            'toggle',
-            'switch'
-        ];
+        skipInputType = Template.afFormGroup_materialize.skipInputType;
         type = AutoForm.getInputType(this);
         result = !_.contains(skipInputType, type);
         return result;
     },
-    skipLabel: function() {
+    skipLabel: function () {
         var result, skipLabelTypes, type;
-        skipLabelTypes = [
-            'checkbox',
-            'checkbox-group',
-            'boolean-checkbox',
-            'select-radio',
-            'select-checkbox-inline',
-            'select-radio-inline',
-            'boolean-radio',
-            'toggle',
-            'switch'
-        ];
+        skipLabelTypes = Template.afFormGroup_materialize.skipLabelTypes;
         type = AutoForm.getInputType(this);
         result = this.skipLabel || _.contains(skipLabelTypes, type);
         return result;
     }
 });
 
-Template.afFormGroup_materialize.rendered = function() {
+Template.afFormGroup_materialize.skipActiveLabelTypes = [
+    'select',
+    'checkbox',
+    'checkbox-group',
+    'boolean-checkbox',
+    'select-radio',
+    'select-checkbox-inline',
+    'select-radio-inline',
+    'boolean-radio',
+    'toggle',
+    'switch'
+];
+Template.afFormGroup_materialize.rendered = function () {
     var formId;
     formId = AutoForm.getFormId();
-    this.autorun((function(_this) {
-        return function() {
+    this.autorun((function (_this) {
+        return function () {
             var value = AutoForm.getFieldValue(_this.data.name, formId);
             var inputValue = AutoForm.getInputValue(_this.find('input'));
             var type = AutoForm.getInputType(_this.data);
             var placeholder = _this.data.afFieldInputAtts.placeholder;
-            var skipActiveLabelTypes = [
-                'select',
-                'checkbox',
-                'checkbox-group',
-                'boolean-checkbox',
-                'select-radio',
-                'select-checkbox-inline',
-                'select-radio-inline',
-                'boolean-radio',
-                'toggle',
-                'switch'
-            ];
+            var skipActiveLabelTypes = Template.afFormGroup_materialize.skipActiveLabelTypes;
 
             if (!_.contains(skipActiveLabelTypes, type)) {
                 if (!!value || !!inputValue || !!placeholder) {
